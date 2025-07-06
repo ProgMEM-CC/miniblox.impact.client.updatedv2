7228f8df606ff536cc5831863b5a4ff6eb3c03b9
ProgMEMCC <progmem.cc@gmail.com>
2025-07-06 19:07:48 +0800

    Revert "Update tampermonkey.js"
    This reverts commit ed9afc0c9f55171ba87c9edd8ac4fa4e605aca50.

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Impact For Miniblox
 // @namespace    he557, ProgMEM-CC
-// @version      1.1.7-beta
+// @version      1.1.5
 // @description  Hacks for Miniblox (haha vector) Lol
 // @author       7GrandDad, Emmaa, ProgMEM-CC, ModuleMaster64
 // @match        https://miniblox.io/*
 // @match        https://miniblox.org/*
 // @match        https://miniblox.online/*
-// @icon         https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/beta/favicon.png
+// @icon         https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/favicon.png
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        unsafeWindow
-// @require      https://raw.githubusercontent.com/ProgMEM-CC/miniblox.impact.client.updatedv2/refs/heads/beta/vav4inject.js
+// @require      https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/vav4inject.js
 // @run-at       document-start
 // ==/UserScript==
 
