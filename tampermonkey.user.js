0dd61f4e37bc66fabd3863ba8c2ed318a374ae6b
ProgMEMCC <progmem.cc@gmail.com>
2025-07-06 20:41:13 +0800

    Update tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -3,7 +3,7 @@
 // @namespace    he557, ProgMEM-CC
 // @version      1.1.5
 // @description  Hacks for Miniblox (haha vector) Lol
-// @author       7GrandDad, Emmaa, ProgMEM-CC, ModuleMaster64
+// @author       7GrandDad, ProgMEM-CC, ModuleMaster64, he557
 // @match        https://miniblox.io/*
 // @match        https://miniblox.org/*
 // @match        https://miniblox.online/*
@@ -16,5 +16,47 @@
 // ** Important For Updating **
 // @updateURL    https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
 // @downloadURL    https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
+// @license      AGPL-3.0-only
+// 
+// ============================================== The Dev Team ===============================================
+//  ProgMEM-CC (github.com/ProgMEM-CC)
+//  ModuleMaster64 (github.com/ModuleMaster64)
+//
+//
+//  Pls, we may need more devs
+//  
+//
+// =============================================== Credits To ================================================
+//    7GrandDad, the OG Creator of the Vape v4 hacked client  for miniblox
+//    he557, adding bypasses and and pushing through, making the client work
+//    ProgMEM-CC, the upgrader and reviver of the client as Impact V4 for miniblox
+//    ModuleMaster64, the one who never fails to amaze, and also the creator of the ClickGUI
+//    RealPacket (codeberg, https://codeberg.org/RealPacket), for the inspiration of reviving the hacks
+//
+//
+//
+//
+// ==================================== Extra Credits To =====================================================
+//    The js.org team, who kindly provided us with the subdomain impactminiblox.js.org for the project site
+//    Vercel.app, which allowed me to have an alternative subdomain at minibloxhacks.vercel.app
+//    Netlify.app, which also allowed me to have an alternative subdomain at minibloxhacks.netlify.app
+//
+//
+// ============================================ LICENSE ======================================================
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License version 3
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>,
+// or see the project page of Impact V4 for miniblox.
+//
+//
+//
 // ==/UserScript==
 
