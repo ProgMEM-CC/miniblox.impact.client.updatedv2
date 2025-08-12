6a21cbe3a8b9a9268bcc4361195b23155628cf99
M0Dul2Mast2r64 <192749026+ModuleMaster64@users.noreply.github.com>
2025-08-12 14:22:41 +0100

    Update tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Impact For Miniblox
 // @namespace    he557, ProgMEM-CC, ModuleM64
-// @version      1.4.1
+// @version      1.4.3
 // @description  The ultimate MiniBlox hacked client. Built for stealth, speed, and total domination. Fully dark-mode optimized with a modern UI and high performance.
 // @author       7GrandDad, ProgMEM-CC, ModuleMaster64, he557, RealPacket/DataM0del (codeberg/Github names)
 // @match        https://miniblox.io/*
@@ -13,9 +13,9 @@
 // @grant        unsafeWindow
 // @require      https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/vav4inject.js
 // @run-at       document-start
-// ** Important For Updating **
+// ** This is **Important** For Updating **
 // @updateURL    https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
-// @downloadURL    https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
+// @downloadURL  https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
 // @license      AGPL-3.0-only
 // 
 // =========================================== EULA ==========================================================
@@ -30,14 +30,14 @@
 //  DataM0del (github.com/datam0del)
 //
 //
-//  Pls, we may need more devs
+//  Pls, we may need more devs!
 //  
 //
 // =============================================== Credits To ================================================
 //    7GrandDad, the OG Creator of the Vape v4 hacked client  for miniblox
 //    he557, adding bypasses and and pushing through, making the client work
 //    ProgMEM-CC, the upgrader and reviver of the client as Impact V4 for miniblox
-//    ModuleMaster64, the one who never fails to amaze, and also the creator of the ClickGUI
+//    @ModuleMaster64, the one who never fails to amaze, and also the creator of the ClickGUI and improving/adding modules!
 //    RealPacket (codeberg, https://codeberg.org/RealPacket), for the inspiration of reviving the hacks
 //
 //
