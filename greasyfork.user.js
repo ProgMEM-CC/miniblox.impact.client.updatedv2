806aa45c29fae03ea5c539ce8bd4366aff6ae3cc
ProgMEMCC <progmem.cc@gmail.com>
2025-08-18 12:13:47 +0800

    Update greasyfork.user.js
    Signed-off-by: ProgMEMCC <progmem.cc@gmail.com>

--- greasyfork.user.js
+++ greasyfork.user.js
@@ -1,1 +1,68 @@
-
+// ==UserScript==
+// @name         Impact For Miniblox
+// @namespace    he557, ProgMEM-CC, ModuleM64, he557, 7GrandDad, BlueDragon7327, DataM0del/RealPacket
+// @version      1.4.4
+// @description  The ultimate MiniBlox hacked client. Built for stealth, speed, and total domination. Fully dark-mode optimized with a modern UI and high performance.
+// @author       7GrandDad, ProgMEM-CC, ModuleMaster64, he557, RealPacket/DataM0del (Codeberg/Github names), BlueDragon7327
+// @match        https://miniblox.io/*
+// @match        https://miniblox.org/*
+// @match        https://miniblox.online/*
+// @icon         https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/favicon.png
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        unsafeWindow
+// @require      https://cdn.jsdelivr.net/gh/ProgMEM-CC/miniblox.impact.client.updatedv2@refs/heads/main/vav4inject.js
+// @run-at       document-start
+// ** This is Important For Updating **
+// @updateURL    https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
+// @downloadURL  https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
+// @license      AGPL-3.0-only
+// 
+// =========================================== EULA ==========================================================
+//  Before using, read https://github.com/ProgMEM-CC/miniblox.impact.client.updatedv2/blob/main/EULA.md
+//
+//
+//
+// ============================================== The Dev Team ===============================================
+//  ProgMEM-CC (github.com/ProgMEM-CC)
+//  ModuleMaster64 (github.com/ModuleMaster64)
+//  DataM0del (github.com/datam0del)
+//  BlueDragon7327 (github.com/BlueDragon7327) (**NEW DEV**)
+//
+//
+//  Pls, we may need more devs!
+//  
+//
+// =============================================== Credits To ================================================
+//    7GrandDad, the OG Creator of the Vape v4 hacked client  for miniblox
+//    he557, adding bypasses and and pushing through, making the client work
+//    ProgMEM-CC, the upgrader and reviver of the client as Impact V4 for miniblox
+//    @ModuleMaster64, the one who never fails to amaze, and also the creator of the ClickGUI and improving/adding modules!
+//    RealPacket, (codeberg, https://codeberg.org/RealPacket), for the inspiration of reviving the hacks
+//    BlueDragon7327,
+//
+//
+//
+// ==================================== Extra Credits To =====================================================
+//    The js.org team, who kindly provided us with the subdomain impactminiblox.js.org for the project site
+//    Vercel.app, which allowed me to have an alternative subdomain at minibloxhacks.vercel.app
+//    Netlify.app, which also allowed me to have an alternative subdomain at minibloxhacks.netlify.app
+//
+//
+// ============================================ LICENSE ======================================================
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License version 3
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>,
+// or see the project page of Impact V4 for miniblox.
+//
+//
+//
+// ==/UserScript==
