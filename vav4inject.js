a23ab74325caf0e354ea87a4dec8b3a5cec5eb43
M0Dul2Mast2r64 <192749026+ModuleMaster64@users.noreply.github.com>
2025-09-02 21:40:37 +0100

    Update vav4inject.js
    Signed-off-by: M0Dul2Mast2r64 <192749026+ModuleMaster64@users.noreply.github.com>

--- vav4inject.js
+++ vav4inject.js
@@ -5,7 +5,7 @@ let replacements = {};
 let dumpedVarNames = {};
 const storeName = "a" + crypto.randomUUID().replaceAll("-", "").substring(16);
 const vapeName = crypto.randomUUID().replaceAll("-", "").substring(16);
-const VERSION = "4.0.3";
+const VERSION = "4.0.4";
 
 // ANTICHEAT HOOK
 function replaceAndCopyFunction(oldFunc, newFunc) {
