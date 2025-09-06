3127d332f27d8267c6504c09c8273a1e3d210791
ProgMEMCC <progmem.cc@gmail.com>
2025-09-06 13:37:45 +0800

    Update vav4inject.js
    Signed-off-by: ProgMEMCC <progmem.cc@gmail.com>

--- vav4inject.js
+++ vav4inject.js
@@ -5,7 +5,7 @@ let replacements = {};
 let dumpedVarNames = {};
 const storeName = "a" + crypto.randomUUID().replaceAll("-", "").substring(16);
 const vapeName = crypto.randomUUID().replaceAll("-", "").substring(16);
-const VERSION = "4.0.4";
+const VERSION = "5.0.0";
 
 // ANTICHEAT HOOK
 function replaceAndCopyFunction(oldFunc, newFunc) {
