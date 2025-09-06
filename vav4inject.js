03117ec07ad2dd920660e2c4628e37cb92f0e2b3
ProgMEMCC <progmem.cc@gmail.com>
2025-09-06 13:04:28 +0800

    Update vav4inject.js
    Signed-off-by: ProgMEMCC <progmem.cc@gmail.com>

--- vav4inject.js
+++ vav4inject.js
@@ -782,7 +782,44 @@ h.addVelocity(-Math.sin(this.yaw) * g * .5, .1, -Math.cos(this.yaw) * g * .5);
 			flybypass = fly.addoption("Bypass", Boolean, true);
 			flyvalue = fly.addoption("Speed", Number, 2);
 			flyvert = fly.addoption("Vertical", Number, 0.7);
+			
+   
+   
+   
+   			let jumpflyvalue, jumpflyvert, jumpFlyUpMotion, jumpFlyGlide;
+			// JumpFly
+			const jumpfly = new Module("JumpFly", function(callback) {
+				if (callback) {
+					let ticks = 0;
+					tickLoop["JumpFly"] = function() {
+						ticks++;
+						const dir = getMoveDirection(jumpflyvalue[1]);
+						player.motion.x = dir.x;
+						player.motion.z = dir.z;
+						const goUp = keyPressedDump("space");
+						const goDown = keyPressedDump("shift");
+						if (goUp || goDown) {
+							player.motion.y = goUp ? jumpflyvert[1] : -jumpflyvert[1];
+						} else {
+							player.motion.y = (ticks < 18 && ticks % 6 < 4 ? jumpFlyUpMotion[1] : jumpFlyGlide[1]);
+						}
+					};
+				}
+				else {
+					delete tickLoop["JumpFly"];
+					if (player) {
+						player.motion.x = Math.max(Math.min(player.motion.x, 0.3), -0.3);
+						player.motion.z = Math.max(Math.min(player.motion.z, 0.3), -0.3);
+					}
+				}
+			});
+			jumpflyvalue = jumpfly.addoption("Speed", Number, 2);
+			jumpFlyGlide = jumpfly.addoption("GlideValue", Number, -0.27);
+			jumpFlyUpMotion = jumpfly.addoption("UpMotion", Number, 4);
+			jumpflyvert = jumpfly.addoption("Vertical", Number, 0.27);
 
+
+   
 			// InfiniteFly
 			let infiniteFlyVert;
 			const infiniteFly = new Module("InfiniteFly", function(callback) {
@@ -1608,7 +1645,7 @@ const jesus = new Module("Jesus", function(callback) {
       Combat: ["autoclicker", "killaura", "velocity", "wtap"],
       Movement: [
         "scaffold","jesus","phase","nofall","sprint","keepsprint","step",
-        "speed","fly","noslowdown","spiderclimb"
+        "speed","fly","noslowdown","spiderclimb","jumpfly"
       ],
       "Player / Render": [
         "invcleaner","invwalk","autoarmor","ghostjoin",
