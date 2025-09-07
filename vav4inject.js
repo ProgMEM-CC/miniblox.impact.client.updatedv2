7cee743ae6e5a0d0ede94975f8983e995070ff54
ProgMEMCC <progmem.cc@gmail.com>
2025-09-07 14:05:59 +0800

    Update vav4inject.js
    Signed-off-by: ProgMEMCC <progmem.cc@gmail.com>

--- vav4inject.js
+++ vav4inject.js
@@ -616,29 +616,6 @@ h.addVelocity(-Math.sin(this.yaw) * g * .5, .1, -Math.cos(this.yaw) * g * .5);
 
 
 
-			// SpeedBETA
-   			const speedbeta = new Module("SpeedBETA", function(callback){
-	  			if (callback){
-	  				tickLoop["SpeedBETA"] = function() {
-	   					                        if (true) {
-                            const offsets = [.51, .5, .49, -.49, 0];
-                            for (const offset of offsets) {
-                                const pos = {
-                                    x: player.pos.x,
-                                    y: player.pos.y + offset,
-                                    z: player.pos.z
-                                };
-                                ClientSocket.sendPacket(new SPacketPlayerPosLook({
-                                    pos: pos,
-									onGround: false
-                                }));
-                            }
-                        }
-					};
-	 			}
-	 			else delete tickLoop["SpeedBETA"]
-	 		});
-
 
 			// Killaura
 			let attackDelay = Date.now();
