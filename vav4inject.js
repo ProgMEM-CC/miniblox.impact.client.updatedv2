dbaa44ffc00b71e7c86d974d851ca8f8803de931
DataModel <183248792+DataM0del@users.noreply.github.com>
2025-09-07 13:50:14 -0500

    fix(NoFall): "even if fly is on", not "even if fly is off" [skip ci]
    skipping CI because this just changes a comment... lol
    
    Signed-off-by: DataModel <183248792+DataM0del@users.noreply.github.com>

--- vav4inject.js
+++ vav4inject.js
@@ -623,7 +623,7 @@ h.addVelocity(-Math.sin(this.yaw) * g * .5, .1, -Math.cos(this.yaw) * g * .5);
 				let shouldDesync = false;
 				tickLoop["NoFall"] = function() {
 					if (!desync && shouldDesync) desync = true;
-	 				// this will force desync off even if fly is off, but I'm too lazy to make an entire priority system
+	 				// this will force desync off even if fly is on, but I'm too lazy to make an entire priority system
 	  				// or something just to fix the 0 uses of fly while you're on the ground
 	 				else if (player.onGround && shouldDesync && desync) desync = false;
 	  				shouldDesync = !player.onGround && player.motionY < -0.6 && player.fallDistance >= 2.5;
