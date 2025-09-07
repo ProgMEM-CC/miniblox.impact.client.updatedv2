aaf99d8f6da8537ae4b9f8d2395697cd8b683810
ProgMEMCC <progmem.cc@gmail.com>
2025-09-07 13:05:06 +0800

    Implement NoFallBeta module with extraY option
    Added NoFallBeta module with extraY option to prevent fall damage.
    
    Signed-off-by: ProgMEMCC <progmem.cc@gmail.com>

--- vav4inject.js
+++ vav4inject.js
@@ -555,6 +555,28 @@ h.addVelocity(-Math.sin(this.yaw) * g * .5, .1, -Math.cos(this.yaw) * g * .5);
 			const velocity = new Module("Velocity", function() {});
 			velocityhori = velocity.addoption("Horizontal", Number, 0);
 			velocityvert = velocity.addoption("Vertical", Number, 0);
+			// Nofall beta?
+   						let noFallExtraYBeta;
+			const NoFallBeta = new Module("NoFallBeta", function(callback) {
+				if (callback) {
+					tickLoop["NoFallBeta"] = function() {
+						// check if the player is falling and above a block
+						// player.fallDistance = 0;
+						const boundingBox = player.getEntityBoundingBox();
+						const clone = boundingBox.min.clone();
+						clone.y -= noFallExtraYBeta[1];
+						const block = rayTraceBlocks(boundingBox.min, clone, true, false, false, game.world);
+						if (block) {
+							sendY = player.pos.y + noFallExtraYBeta[1];
+						}
+					}
+				} else {
+					delete tickLoop["NoFallBeta"];
+				}
+			});
+			noFallExtraYBeta = NoFallBeta.addoption("extraY", Number, .41);
+
+
 
 			// NoFall
 			new Module("NoFall", function(callback) {
