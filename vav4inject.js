7ef0a75698f5389032668d0d920ba7b61d6d6dd5
DataModel <183248792+DataM0del@users.noreply.github.com>
2025-09-09 22:30:06 -0500

    fix(vav4inject.js): the syntax error is apparently a invisible character
    :broken_heart:
    
    Signed-off-by: DataModel <183248792+DataM0del@users.noreply.github.com>

--- vav4inject.js
+++ vav4inject.js
@@ -502,7 +502,7 @@ h.addVelocity(-Math.sin(this.yaw) * g * .5, .1, -Math.cos(this.yaw) * g * .5);
 		if (enabledModules["FilterBypass"] && !this.isInputCommandMode) {
 			const words = this.inputValue.split(" ");
 			let newwords = [];
-			for(const word of words) newwords.push(word.charAt(0) + ‎'\\\\' + word.slice(1));
+			for(const word of words) newwords.push(word.charAt(0) + '\\\\' + word.slice(1));
 			this.inputValue = newwords.join(' ');
 		}
 	`);
