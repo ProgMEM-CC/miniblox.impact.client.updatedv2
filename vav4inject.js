a23628ab78bc0a1969d2109e74fb00cb825cece2
M0Dul2Mast2r64 <192749026+ModuleMaster64@users.noreply.github.com>
2025-09-15 21:44:12 +0100

    Update vav4inject.js
    bump VERSION to 5.4.0!
    
    Signed-off-by: M0Dul2Mast2r64 <192749026+ModuleMaster64@users.noreply.github.com>

--- vav4inject.js
+++ vav4inject.js
@@ -5,7 +5,7 @@ let replacements = {};
 let dumpedVarNames = {};
 const storeName = "a" + crypto.randomUUID().replaceAll("-", "").substring(16);
 const vapeName = crypto.randomUUID().replaceAll("-", "").substring(16);
-const VERSION = "5.3.0";
+const VERSION = "5.4.0";
 
 // ANTICHEAT HOOK
 function replaceAndCopyFunction(oldFunc, newFunc) {
