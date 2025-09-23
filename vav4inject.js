2905af91a64e0cb3829966f083257ae62d7bfae7
TheM1ddleM1n <192749026+ModuleMaster64@users.noreply.github.com>
2025-09-23 18:20:15 +0100

    Update vav4inject.js
    added a new module - LongJump
    
    Signed-off-by: TheM1ddleM1n <192749026+ModuleMaster64@users.noreply.github.com>

--- vav4inject.js
+++ vav4inject.js
@@ -1643,6 +1643,48 @@ const jesus = new Module("Jesus", function(callback) {
         delete tickLoop["Jesus"];
     }
 });
+
+// LongJump (Created by TheM1ddleM1n or ModuleMaster64 lol)
+let ljpower, ljboost, ljdesync;
+const longjump = new Module("LongJump", function(callback) {
+    if (!callback) {
+        delete tickLoop["LongJump"];
+        desync = false;
+        return;
+    }
+
+    desync = ljdesync[1];
+    let jumping = false;
+    let boostTicks = 0;
+
+    tickLoop["LongJump"] = function() {
+        if (!player) return;
+
+        // Detect jump key
+        if (keyPressedDump("space") && player.onGround && !jumping) {
+            jumping = true;
+            boostTicks = ljboost[1];
+            player.motion.y = 0.42; // vanilla mc jump power lol
+        }
+
+        if (jumping) {
+            const dir = getMoveDirection(ljpower[1]);
+            player.motion.x = dir.x;
+            player.motion.z = dir.z;
+
+            boostTicks--;
+            if (boostTicks <= 0 || player.onGround) {
+                jumping = false;
+            }
+        }
+    };
+});
+
+// Options
+ljpower  = longjump.addoption("Power", Number, 0.6);   // horizontal boost
+ljboost  = longjump.addoption("BoostTicks", Number, 10); // how long boost lasts
+ljdesync = longjump.addoption("Desync", Boolean, true);  // toggle desync mode
+
 			globalThis.${storeName}.modules = modules;
 			globalThis.${storeName}.profile = "default";
 		})();
