c4103c2ea72617002e346b43233266e02541b7ad
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2025-09-29 19:58:59 +0100

    Update vav4inject.js
    Bumped version from `v5.8` to `v5.9`
    
    Signed-off-by: TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>

--- vav4inject.js
+++ vav4inject.js
@@ -5,7 +5,7 @@ let replacements = {};
 let dumpedVarNames = {};
 const storeName = "a" + crypto.randomUUID().replaceAll("-", "").substring(16);
 const vapeName = crypto.randomUUID().replaceAll("-", "").substring(16);
-const VERSION = "5.8";
+const VERSION = "5.9";
 
 // ANTICHEAT HOOK
 function replaceAndCopyFunction(oldFunc, newFunc) {
