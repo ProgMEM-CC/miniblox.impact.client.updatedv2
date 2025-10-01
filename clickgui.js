1d579a4f5e2d3ee5a133dc774d596608814d9f50
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2025-10-01 18:25:34 +0100

    Update clickgui.js
    Signed-off-by: TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>

--- clickgui.js
+++ clickgui.js
@@ -1,4 +1,4 @@
 // clickgui.js
-// Placeholder for GUI refactor — see vav4inject.js for current implementation
+// Placeholder for GUI refactor — see vav4inject.js for the current implementation!
 
 console.log("THIS IS A PLACEHOLDER. WHAT ARE YOU DOING HERE!")
