9e46a58eb747915abd79b82b18398fd528ba4818
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2025-10-27 21:13:37 +0000

    Update test-for-scriptsystem.js
    Signed-off-by: TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>

--- test-for-scriptsystem.js
+++ test-for-scriptsystem.js
@@ -1,12 +1,12 @@
 // Simple test script
 new Module("TestScript", function (enabled) {
     if (enabled) {
-        console.log("TestScript enabled!");
+        console.log("TestScript has been enabled!");
         tickLoop["TestScript"] = function () {
             console.log("TestScript tickLoop")
         };
     } else {
-        console.log("TestScript disabled!");
+        console.log("TestScript has been disabled!");
         delete tickLoop["TestScript"];
     }
 });
