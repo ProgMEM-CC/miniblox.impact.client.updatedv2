07d378e68b7c760f0b8f5bf1931c601d49d4973f
Flagged DataM0del <248750512+6x67@users.noreply.github.com>
2025-12-21 16:44:26 -0600

    Update tampermonkey.user.js
    Signed-off-by: Flagged DataM0del <248750512+6x67@users.noreply.github.com>

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Impact For Miniblox!
-// @namespace    ProgMEM-CC, TheM1ddleM1n, he557, 7GrandDad, BlueDragon7327, DataM0del/RealPacket, ekusos22, whoppps, annepaurush
+// @namespace    https://github.com/ProgMEM-CC
 // @version      6.8.7
 // @description  The ultimate Miniblox hacked client! Built for total domination of Miniblox servers! Fully dark-mode optimized with a modern GUI and high performance!
-// @author       7GrandDad, ProgMEM-CC, TheM1ddleM1n, he557, RealPacket/DataM0del (Codeberg/Github names), BlueDragon7327, whoppps, annepaurush
+// @author       7GrandDad, ProgMEM-CC, TheM1ddleM1n, he557, RealPacket/DataM0del (Codeberg/Github names), BlueDragon7327, whoppps
 // @match        https://miniblox.io/
 // @match        https://miniblox.org/
 // @icon         https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/favicon.png
@@ -12,7 +12,6 @@
 // @grant        unsafeWindow
 // @require      https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/vav4inject.js
 // @run-at       document-start
-// ** This is Important For Updating **
 // @updateURL    https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
 // @downloadURL  https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
 // @license      AGPL-3.0-only
