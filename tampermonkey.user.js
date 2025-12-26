1db5a8a7f35895fbdd8d9312a0a9689eb45a3a9d
TheMiddleMan <192749026+TheM1ddleM1n@users.noreply.github.com>
2025-12-26 18:19:32 +0000

    Bump version to 6.8.8 and clean up comments
    Updated version number from 6.8.7 to 6.8.8 and removed extensive comments. because i will put this into /docs and will format into readme shortly
    
    Signed-off-by: TheMiddleMan <192749026+TheM1ddleM1n@users.noreply.github.com>

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Impact For Miniblox!
 // @namespace    https://github.com/ProgMEM-CC
-// @version      6.8.7
+// @version      6.8.8
 // @description  The ultimate Miniblox hacked client! Built for total domination of Miniblox servers! Fully dark-mode optimized with a modern GUI and high performance!
 // @author       7GrandDad, ProgMEM-CC, TheM1ddleM1n, he557, RealPacket/DataM0del (Codeberg/Github names), BlueDragon7327, whoppps
 // @match        https://miniblox.io/
@@ -15,50 +15,6 @@
 // @updateURL    https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
 // @downloadURL  https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
 // @license      AGPL-3.0-only
-// 
-// =========================================== EULA ==========================================================
-//  Before using, read https://github.com/ProgMEM-CC/miniblox.impact.client.updatedv2/blob/main/EULA.md
 //
-// ============================================== The Developer Team: for Impact For Miniblox! ===============================================
-//  ProgMEM-CC (github.com/ProgMEM-CC)
-//  TheM1ddleM1n (github.com/TheM1ddleM1n)
-//  DataM0del (github.com/datam0del)
-//  BlueDragon7327 (github.com/BlueDragon7327) 
-//  dtkiller-jp (github.com/dtkiller-jp)
-//  whoppps (github.com/whoppps)
-//
-//  i dont think we need anymore devs lol :trollface: (unless required)
-//  
-// =============================================== Credits To ================================================
-//    7GrandDad, the OG Creator of the Vape V4 hacked client for Miniblox!
-//    he557, doing absolutely nothing other than forking the already working Vape for Miniblox fork!
-//    ProgMEM-CC, the upgrader and reviver of the client as Impact V4 for Miniblox!
-//    TheM1ddleM1n, the one who never fails to amaze, creating a LB themed ClickGUI and creating new modules!
-//    RealPacket, (codeberg: https://codeberg.org/RealPacket) for the inspiration of reviving the hacks and making a genius desync for Fly and Nofall, also making .friend command, Antiblind module etc!
-//    BlueDragon7327, Module Researcher and solver!
-//    dtkiller-jp, Bug reporter and solver!
-//    whoppps, issue raiser
-//
-// ==================================== Extra Credits To! =====================================================
-//    The js.org team, who kindly provided us with the subdomain impactminiblox.js.org for the project site
-//    Vercel.app, which allowed me to have an alternative subdomain at minibloxhacks.vercel.app
-//    Netlify.app, which also allowed me to have an alternative subdomain at minibloxhacks.netlify.app
-//
-// ============================================ LICENSE ======================================================
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License version 3
-// as published by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>,
-// or see the project page of Impact V4 for miniblox.
-//
-// For Additional Terms:
-// Please refer to EULA.md
 //
 // ==/UserScript==
