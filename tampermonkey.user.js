bcb959f56cd62259e1d0bec60075d76d18466947
TheMiddleMan <192749026+TheM1ddleM1n@users.noreply.github.com>
2025-12-27 14:37:17 +0000

    Update authors in tampermonkey.user.js
    Signed-off-by: TheMiddleMan <192749026+TheM1ddleM1n@users.noreply.github.com>

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/progmem-cc
 // @version      6.8.8
 // @description  The ultimate Miniblox hacked client which is built for total domination of all Miniblox servers with a fully dark-mode optimized modern GUI and high performance guaranteed.
-// @author       7GrandDadPGN, ProgMEM-CC, TheM1ddleM1n, 6x68 (bab)
+// @author       7GrandDadPGN, ProgMEM-CC, TheM1ddleM1n, 6x68 (bab), dtkiller-jp
 // @match        https://miniblox.io/
 // @match        https://miniblox.org/
 // @icon         https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/favicon.png
