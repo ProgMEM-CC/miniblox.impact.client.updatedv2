51447794078c54026957bb344cfe684b5969dcaa
TheMiddleMan <192749026+TheM1ddleM1n@users.noreply.github.com>
2025-12-27 14:36:29 +0000

    Revise user script metadata for clarity and accuracy
    Updated metadata for the Tampermonkey user script, including name, namespace, description, and author.
    
    Signed-off-by: TheMiddleMan <192749026+TheM1ddleM1n@users.noreply.github.com>

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Impact For Miniblox!
-// @namespace    https://github.com/ProgMEM-CC
+// @name         Impact For Miniblox
+// @namespace    https://github.com/progmem-cc
 // @version      6.8.8
-// @description  The ultimate Miniblox hacked client! Built for total domination of Miniblox servers! Fully dark-mode optimized with a modern GUI and high performance!
-// @author       7GrandDad, ProgMEM-CC, TheM1ddleM1n, he557, RealPacket/DataM0del (Codeberg/Github names), BlueDragon7327, whoppps
+// @description  The ultimate Miniblox hacked client which is built for total domination of all Miniblox servers with a fully dark-mode optimized modern GUI and high performance guaranteed.
+// @author       7GrandDadPGN, ProgMEM-CC, TheM1ddleM1n, 6x68 (bab)
 // @match        https://miniblox.io/
 // @match        https://miniblox.org/
 // @icon         https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/favicon.png
@@ -15,6 +15,4 @@
 // @updateURL    https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
 // @downloadURL  https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
 // @license      AGPL-3.0-only
-//
-//
 // ==/UserScript==
