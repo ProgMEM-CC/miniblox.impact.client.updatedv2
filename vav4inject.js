b6ce94e3ecc70780979007cca99752d1211a0902
Seized and Desisted <249649284+6x68@users.noreply.github.com>
2026-06-14 16:48:26 -0500

    chore: version bump
    Signed-off-by: Seized and Desisted <249649284+6x68@users.noreply.github.com>

--- vav4inject.js
+++ vav4inject.js
@@ -5,7 +5,7 @@ let replacements = {};
 let dumpedVarNames = {};
 const storeName = "a" + crypto.randomUUID().replaceAll("-", "").substring(16);
 const vapeName = crypto.randomUUID().replaceAll("-", "").substring(16);
-const VERSION = "9-FINAL3";
+const VERSION = "9-FINAL4";
 
 // Anticheat hooking
 function replaceAndCopyFunction(oldFunc, newFunc) {
